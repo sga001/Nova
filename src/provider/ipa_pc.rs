dfd57d253a619c57c802b27507b0c8f25023a6af
jkwoods <woodsjk@seas.upenn.edu>
2023-11-09 14:48:21 -0500

    alright, convinced

--- src/provider/ipa_pc.rs
+++ src/provider/ipa_pc.rs
@@ -508,49 +508,6 @@ where
     ))
   }
 
-  /*fn bullet_reduce_verifier(
-    P: &Commitment<G>,
-    P_L: &Commitment<G>,
-    P_R: &Commitment<G>,
-    a_vec: &[G::Scalar],
-    gens: &CommitmentGens<G>,
-    transcript: &mut Transcript,
-  ) -> Result<
-    (
-      Commitment<G>,     // P'
-      Vec<G::Scalar>,    // a_vec'
-      CommitmentGens<G>, // gens'
-    ),
-    NovaError,
-  > {
-    let n = a_vec.len();
-
-    P_L.append_to_transcript(b"L", transcript);
-    P_R.append_to_transcript(b"R", transcript);
-
-    let chal = G::Scalar::challenge(b"challenge_r", transcript);
-
-    //    println!("Challenge in bullet_reduce_verifier {:?}", chal);
-
-    let chal_square = chal * chal;
-    let chal_inverse = chal.invert().unwrap();
-    let chal_inverse_square = chal_inverse * chal_inverse;
-
-    // This takes care of splitting them in half and multiplying left half
-    // by chal_inverse and right half by chal
-    let gens_prime = gens.fold(&chal_inverse, &chal);
-
-    let a_vec_prime = a_vec[0..n / 2]
-      .par_iter()
-      .zip(a_vec[n / 2..n].par_iter())
-      .map(|(a_L, a_R)| *a_L * chal_inverse + chal * *a_R)
-      .collect::<Vec<G::Scalar>>();
-
-    let P_prime = *P_L * chal_square + *P + *P_R * chal_inverse_square;
-
-    Ok((P_prime, a_vec_prime, gens_prime))
-  }*/
-
   /// prover inner product argument
   pub fn prove(
     gens: &CommitmentGens<G>,
@@ -661,6 +618,7 @@ where
     })
   }
 
+  // from Spartan, notably without the zeroizing buffer
   fn batch_invert(inputs: &mut [G::Scalar]) -> G::Scalar {
     // This code is essentially identical to the FieldElement
     // implementation, and is documented there.  Unfortunately,
@@ -708,6 +666,7 @@ where
     ret
   }
 
+  // copied almost directly from the Spartan method, with some type massaging
   fn verification_scalars(
     &self,
     n: usize,
@@ -745,7 +704,7 @@ where
     let challenges_sq = challenges;
     let challenges_inv_sq = challenges_inv;
 
-    // s values inductively - wtf is happening here? check
+    // s values inductively
     let mut s = Vec::with_capacity(n);
     s.push(prod_all_inv);
     for i in 1..n {
@@ -790,18 +749,6 @@ where
     let a_vec = U.a_vec.clone();
 
     // Step 1 in Hyrax's figure 7.
-    /*  for i in 0..self.P_L_vec.len() {
-          let P_L = self.P_L_vec[i].decompress().unwrap();
-          let P_R = self.P_R_vec[i].decompress().unwrap();
-
-          let (P_prime, a_vec_prime, gens_prime) =
-            Self::bullet_reduce_verifier(&P, &P_L, &P_R, &a_vec, &gens, transcript)?;
-
-          P = P_prime;
-          a_vec = a_vec_prime;
-          gens = gens_prime;
-        }
-    */
 
     let (mut u_sq, mut u_inv_sq, s) = self.verification_scalars(n, transcript)?;
 
@@ -841,11 +788,7 @@ where
         self.delta.decompress().unwrap().reinterpret_as_generator(),
       ],
     );
-    /*
-        let P_plus_beta = P * chal + self.beta.decompress().unwrap();
-        let P_plus_beta_to_a = P_plus_beta * a;
-        let left_hand_side = P_plus_beta_to_a + self.delta.decompress().unwrap();
-    */
+
     // g_hat^z1 * g^(a*z1) * h^z2
     let right_hand_side = G::vartime_multiscalar_mul(
       &[self.z_1, (self.z_1 * a), self.z_2],
@@ -855,16 +798,6 @@ where
         gens_y.get_blinding_gen(),
       ],
     );
-
-    /*let g_hat = gens_g; // Check? //CE::<G>::commit(&gens, &[G::Scalar::one()], &G::Scalar::zero());
-
-        let g_to_a = G::vartime_multiscalar_mul(&[a], gens_y.get_gens()); //CE::<G>::commit(&gens_y, &[a], &G::Scalar::zero()); // g^a*h^0 = g^a
-        let h_to_z2 = G::vartime_multiscalar_mul(&[a], gens_y.get_gens()); //CE::<G>::commit(&gens_y, &[G::Scalar::zero()], &self.z_2); // g^0 * h^z2 = h^z2
-
-        let g_hat_plus_g_to_a = g_hat + g_to_a;
-        let val_to_z1 = g_hat_plus_g_to_a * self.z_1;
-        let right_hand_side = val_to_z1 + h_to_z2;
-    */
 
     if left_hand_side == right_hand_side {
       Ok(())
